85e3a531da4f664baf8d9ee83904ff4229157e3c
aqibshabir <aqibfaisalshabir@gmail.com>
2024-01-06 11:53:32 +0000

    created navbar/sidebar for creator dashboard page - skeleton loaders need work

--- app/(dashboard)/u/[username]/layout.tsx
+++ app/(dashboard)/u/[username]/layout.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { getSelfByUsername } from "@/lib/auth-service";
 
 import { Navbar } from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
+import { Container } from "./_components/container";
 
 interface CreatorLayoutProps {
   params: { username: string };
@@ -19,7 +21,10 @@ const CreatorLayout = async ({ params, children }: CreatorLayoutProps) => {
   return (
     <>
       <Navbar />
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        <Container>{children}</Container>
+      </div>
     </>
   );
 };
