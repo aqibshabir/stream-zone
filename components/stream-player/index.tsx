831529fdfc7e546465a60bb8b92d3bb414e5d73d
aqibshabir <aqibfaisalshabir@gmail.com>
2024-01-10 17:22:33 +0000

    created loading/offline components for video component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -5,6 +5,8 @@ import { LiveKitRoom } from "@livekit/components-react";
 
 import { useViewerToken } from "@/hooks/use-viewer-token";
 
+import { Video } from "./video";
+
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
   stream: Stream;
@@ -26,8 +28,14 @@ export const StreamPlayer = ({
 
   return (
     <>
-      <LiveKitRoom token={token} serverUrl={process.env.LIVEKIT_API_WS_URL}>
-        Stream
+      <LiveKitRoom
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+      >
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
       </LiveKitRoom>
     </>
   );
