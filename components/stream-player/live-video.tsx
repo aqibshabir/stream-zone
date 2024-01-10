8b7114519ce0a92a2ba97e83fb84a390137d0a73
aqibshabir <aqibfaisalshabir@gmail.com>
2024-01-10 20:09:48 +0000

    added fullscreen component to live player

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Participant, Track } from "livekit-client";
 import { useTracks } from "@livekit/components-react";
+import { useEventListener } from "usehooks-ts";
+import { FullscreenControl } from "./fullscreen-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -11,6 +13,23 @@ interface LiveVideoProps {
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleFullscreen = () => {
+    if (isFullscreen) {
+      document.exitFullscreen();
+    } else if (wrapperRef?.current) {
+      wrapperRef.current.requestFullscreen();
+    }
+  };
+
+  const handleFullscreenChange = () => {
+    const isCurrentlyFullscreen = document.fullscreenElement !== null;
+    setIsFullscreen(isCurrentlyFullscreen);
+  };
+
+  useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
 
   useTracks([Track.Source.Camera, Track.Source.Microphone])
     .filter((track) => track.participant.identity === participant.identity)
@@ -23,6 +42,14 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   return (
     <div ref={wrapperRef} className="relative h-full flex">
       <video ref={videoRef} width="100%" />
+      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+        <div className="absolute bottom-0 flex h-12 w-full items-center justify-end bg-gradient-to-t from-black/80 px-4">
+          <FullscreenControl
+            isFullscreen={isFullscreen}
+            onToggle={toggleFullscreen}
+          />
+        </div>
+      </div>
     </div>
   );
 };
